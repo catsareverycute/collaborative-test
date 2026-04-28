6ca62dd83e7b429ae985066f447989878ff439f2
BT_C09_15 <mobileappdev@bths.edu>
2026-04-28 09:01:41 -0400

    commit

---
+++ collaborationfun/File.swift
@@ -0,0 +1,16 @@
+import SwiftUI
+
+struct File: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Collab!")
+            Text("hi")
+            Text("Github")
+            Text("Commit")
+        }
+        .padding()
+    }
+}
